31db3c7792ded5d019698440b50d6f6c75f76e84
JoasCamargo <jcravocamargo74@gmail.com>
2022-09-19 10:53:50 -0300

    alteração de nome e foto de perfil e pesquisa de usuários concluída

--- services/uploadImagemCosmic.ts
+++ services/uploadImagemCosmic.ts
@@ -24,6 +24,14 @@ const upload = multer({storage : storage});
 
 const uploadImagemCosmic = async(req : any) => {
     if(req?.file?.originalname){
+
+        if(!req.file.originalname.includes('.png') &&
+            !req.file.originalname.includes('.jpg') &&
+            !req.file.originalname.includes('.jpeg')){
+                throw new Error('Extensão de imagem invalida');
+        }
+
+
         const media_object = {
             originalname: req.file.originalname,
             buffer : req.file.buffer
