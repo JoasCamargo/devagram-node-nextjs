be1d41ca877afb604d115f8933b2dae9fcf1aa78
JoasCamargo <jcravocamargo74@gmail.com>
2022-09-20 12:47:24 -0300

    implementação da API de seguir/desseguir

---
+++ pages/api/seguir.ts
@@ -0,0 +1,63 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { SeguidorModel } from '../../models/SeguidorModel';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+
+const endpointSeguir = 
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+    try{
+        if(req.method === 'PUT'){
+            
+            const {userId, id} = req?.query;
+
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro : 'Usuario logado não encontrado'});
+            }
+
+            const usuarioASerSeguido = await UsuarioModel.findById(id);
+            if(!usuarioASerSeguido){
+                return res.status(400).json({erro : 'Usuario a ser seguido não encontrado'});
+            }
+
+            const euJaSigoEsseUsuario = await SeguidorModel
+                .find({usuarioId: usuarioLogado._id, usuarioSeguidoId : usuarioASerSeguido._id});
+            if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0){
+
+                euJaSigoEsseUsuario.forEach(async(e : any) => 
+                await SeguidorModel.findByIdAndDelete({_id : e._id}));
+
+                usuarioLogado.seguindo--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+                usuarioASerSeguido.seguidores--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+
+                return res.status(200).json({msg : 'Deixou de seguir usuario com sucesso'});
+            }else{
+                const seguidor = {
+                    usuarioId : usuarioLogado._id,
+                    usuarioSeguidoId : usuarioASerSeguido._idcle
+                };
+                await SeguidorModel.create(seguidor);
+
+                usuarioLogado.seguindo++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+
+
+                usuarioASerSeguido.seguidores++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+
+                return res.status(200).json({msg :  'Usuario seguido com sucesso'});
+            }
+        }
+
+        return res.status(405).json({erro : 'Metodo informado não existe'});
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({erro : 'Não foi possicel seguir/deseguir o usuario informado'});
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(endpointSeguir));
